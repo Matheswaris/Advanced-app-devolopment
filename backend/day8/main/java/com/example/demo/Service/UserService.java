787c5c00722ec2e54efb04d18bd57e5402501b51
Matheswaris <119093662+Matheswaris@users.noreply.github.com>
2024-02-09 12:21:04 +0530

    Add files via upload

---
+++ backend/day8/main/java/com/example/demo/Service/UserService.java
@@ -0,0 +1,37 @@
+package com.example.demo.Service;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.Model.Users;
+import com.example.demo.Repository.UserRepo;
+
+//import com.elderly.elderly.entity.Users;
+//import com.elderly.elderly.repository.UserRepo;
+
+
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepo ur;
+     
+     
+     public boolean AddUser(Users ue)
+     {
+          ur.save(ue);
+          return true;
+     }
+     public List<Users> getUser()
+     {
+          return ur.findAll();
+     }
+     public Optional<Users> getById(String id)
+     {
+    	 return ur.findById(id);
+     }
+}
