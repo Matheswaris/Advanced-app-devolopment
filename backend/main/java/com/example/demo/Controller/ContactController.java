57742d992f00c7e8f274921c9013518558d89388
Matheswaris <119093662+Matheswaris@users.noreply.github.com>
2024-02-09 10:46:44 +0530

    Add files via upload

---
+++ backend/main/java/com/example/demo/Controller/ContactController.java
@@ -0,0 +1,25 @@
+package com.example.demo.Controller;
+
+import com.example.demo.Service.ContactService;
+import com.example.demo.dto.ContactDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/contacts")
+public class ContactController {
+
+    @Autowired
+    private ContactService contactService;
+
+    @PostMapping("/post")
+    public ResponseEntity<ContactDto> createContact(@RequestBody ContactDto contactDto) {
+        ContactDto createdContact = contactService.createContact(contactDto);
+        return new ResponseEntity<>(createdContact, HttpStatus.CREATED);
+    }
+
+    // If you need to implement other CRUD operations, you can add them here
+
+}
